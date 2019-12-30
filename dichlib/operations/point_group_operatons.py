efe8e1d35ac769da790035193471bc8954e2cd3d
kei0822kei <mizokami.keiyu@gmail.com>
2019-12-30 16:44:40 +0900

    add 'get_Ww' and 'get_detailed_spg_operations

---
+++ dichlib/operations/point_group_operatons.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This file provides various kind of calculations about point group oprations.
+"""
+import numpy as np
+
+def get_all_pog_operations(coord_sys):
+    """
+    get all point group operations specified coordinate system
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+        pog_operations : dict
+            all point group operations
+
+        Notes
+        -----
+        reference: ITA p.14 Table 1.2.2.1 and 1.2.2.2
+
+        Raises
+        ------
+        unexpected coodinate system is specifed
+    """
+    def __add_dict(symbol,
+                   geometric_element,
+                   direction,
+                   transformed_coord,
+                   matrix):
+        pog_operations[symbol] = {
+                'symbol': symbol,
+                'geometric_element': geometric_element,
+                'direction': direction,
+                'transformed_coord': transformed_coord,
+                'matrix': matrix
+            }
+
+    except_hexagol = ['cubic',
+                      'tetragonal',
+                      'orthorhombic',
+                      'monoclinic',
+                      'triclinic',
+                      'rhombohedral']
+
+    pog_operations = {}
+    if coord_sys in except_hexagol:
+        __add_dict(
+                symbol = '1',
+                geometric_element = None,
+                direction = None,
+                transformed_coord = 'xyz',
+                matrix = np.array([[1, 0, 0],
+                                   [0, 1, 0],
+                                   [0, 0, 1]])
+            )
+        __add_dict(
+                symbol = '3+',
+                geometric_element = 'xxx',
+                direction = np.array([1,1,1]),
+                transformed_coord = 'zxy',
+                matrix = np.array([[0, 0, 1],
+                                   [1, 0, 0],
+                                   [0, 1, 0]])
+            )
+
+    return pog_operations
---
+++ dichlib/operations/space_group_operatons.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This file provides various kind of calculations about space group oprations.
+"""
+import numpy as np
+
+def get_Ww(W, w):
+    """
+    get 4 x 4 matrix Ww from the operation
+    of rotation part W and translation part w
+
+        Parameters
+        ----------
+        W : np.array
+            rotation part (3 x 3) of space group operation
+        w : np.array
+            translation part (3) of space group operation
+
+        Returns
+        -------
+        Ww : np.array
+            concatted space group operaton (4 x 4)
+
+        Notes
+        -----
+
+        Raises
+        ------
+    """
+    Ww = np.concatenate([W, w.reshape(3,1)], axis=1)
+    Ww = np.concatenate([Ww, np.array([0,0,0,1]).reshape(1,4)], axis=0)
+    return Ww
+
+def get_detailed_spg_operation(W, w):
+    """
+    get detailed space group operation information
+    such as intrinsic part of translation operation
+
+        Parameters
+        ----------
+        W : np.array
+            rotation part (3 x 3) of space group operation
+        w : np.array
+            translation part (3) of space group operation
+
+        Returns
+        -------
+        spg_operation : dict
+            informations about a given space group operation
+
+        Notes
+        -----
+
+        Raises
+        ------
+        ValueError
+            could not detect index of the operation
+    """
+    epsilon = 1e-8
+    n_max = 20
+
+    Ww_n = np.eye(4, dtype=float)
+    Ww = get_Ww(W, w)
+    for n in range(1, n_max+1):
+        Ww_n = np.dot(Ww_n, Ww)
+        if np.all(abs(Ww_n[:3, :3] - np.eye(3)) < epsilon):
+            break
+        if n == n_max:
+            raise ValueError("could not detect index of the operation")
+    w_intr = Ww[:3, 3] / n
+    spg_operation = {
+        'W': W,
+        'w': w,
+        'w_intr': w_intr,
+        'w_loc': w - w_intr,
+        'W_index': n
+    }
+    return spg_operation
